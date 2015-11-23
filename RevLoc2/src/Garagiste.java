b9fb2a13199d574954ec46bb7fd7403a632d11b2
Rahma <bouraoui.rahma.sio@gmail.com>
2015-11-23 15:28:21 +0100

    Getters&Setters

--- RevLoc2/src/Garagiste.java
+++ RevLoc2/src/Garagiste.java
@@ -9,6 +9,22 @@ public class Garagiste {
 		this.nom = nom;
 		this.adresse = adresse;
 	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
 
 	@Override
 	public String toString() {
